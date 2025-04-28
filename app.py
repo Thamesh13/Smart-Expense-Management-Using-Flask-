34e5ac68d0bc07640e530bd985f84dd711aef7fa
Thamesh13 <thamesh13082002@gmail.com>
2025-04-28 13:17:26 +0530

    second commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# APP for Flask
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
